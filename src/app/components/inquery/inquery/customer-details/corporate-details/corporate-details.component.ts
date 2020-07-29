51b7c8c6504909aa03f4ecee6d17aa47938d90b4
Thilini <thiliniomattage@outlook.com>
2020-07-29 15:38:36 -0700

    add coporate component UI

--- src/app/components/inquery/inquery/customer-details/corporate-details/corporate-details.component.ts
+++ src/app/components/inquery/inquery/customer-details/corporate-details/corporate-details.component.ts
@@ -3,6 +3,7 @@ import {FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormAr
 import {PasswordHash} from '../../../../../auth/auth-login/password-hash';
 import {Router} from '@angular/router';
 import {FormValidationHelpers} from '../../../../../shared/helpers/form-validation-helpers';
+import { TextBoxTypes } from 'src/app/shared/services/common/enum';
 
 
 
@@ -13,7 +14,7 @@ import {FormValidationHelpers} from '../../../../../shared/helpers/form-validati
   styleUrls: ['./corporate-details.component.scss']
 })
 
-export class CorporateDetailsComponent{
+export class CorporateDetailsComponent implements OnInit{
   constructor(
     private passwordHash: PasswordHash,
     private router:Router,
@@ -21,8 +22,86 @@ export class CorporateDetailsComponent{
     private formValidationHelper: FormValidationHelpers
   ) {}
 
+  ngOnInit(): void {
+    this.getCorporateForm();
+  }
 
+  corporateForm : FormGroup;
+  TextBoxTypes: typeof TextBoxTypes = TextBoxTypes;
 
+  getCorporateForm(){
+    this.corporateForm = this.formBuilder.group({
+      companyName : ['', Validators.required],
+      companyRegistrationNo : ['', Validators.required],
+      email : ['', [Validators.required, Validators.email]],
+      streetAddressLineOne : ['', Validators.required],
+      streetAddressLineTwo  : ['', Validators.required],
+      country : ['', Validators.required],
+      city : ['', Validators.required],
+      zipCode : ['', Validators.required],
+      // handlingCompany : ['', Validators.required],
+      taxNumber : ['', Validators.required],
+      vatNumber : ['', Validators.required],
+      contactPerson : ['', Validators.required],
+      contactNo : ['', Validators.required],
 
+    })
+  }
+
+  HandlingCompany = [
+    {id : 1, name:'Ingenii'},
+    {id : 2, name:'Dimo'}
+  ]
+
+  get companyName() {
+    return this.corporateForm.get('companyName');
+  }
+  get  companyRegistrationNo() {
+    return this.corporateForm.get('companyRegistrationNo');
+  }
+  get contactPerson() {
+    return this.corporateForm.get('contactPerson');
+  }
+  get contactNo() {
+    return this.corporateForm.get('contactNo');
+  }
+  get designation() {
+    return this.corporateForm.get(' designation');
+  }
+  get masterAccNo() {
+    return this.corporateForm.get('masterAccNo');
+  }
+  get vatNumber() {
+    return this.corporateForm.get('vatNumber');
+  }
+  get email() {
+    return this.corporateForm.get('email');
+  }
+  get addressLine1() {
+    return this.corporateForm.get('addressLine1');
+  }
+  get streetAddressLineOne() {
+    return this.corporateForm.get('streetAddressLineOne');
+  }
+  get streetAddressLineTwo() {
+    return this.corporateForm.get('streetAddressLineTwo');
+  }
+
+  get country() {
+    return this.corporateForm.get('country');
+  }
+  get city(){
+    return this.corporateForm.get('city');
+  }
+  get zipCode(){
+    return this.corporateForm.get('zipCode');
+  }
+ get taxNumber(){
+    return this.corporateForm.get('taxNumber');
+  }
+
+  saveCorporteDetails(){
+
+  }
 
 }
