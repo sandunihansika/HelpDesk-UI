21e9990042605fd69c8844202c074e88a0c9434a
dilina <dilina.17@outlook.com>
2020-08-14 17:10:42 +0530

    customer-handling form updated with fixing some issues.

--- src/app/components/inquery/inquery/inquery-table/customer-handling/customer-handling.component.ts
+++ src/app/components/inquery/inquery/inquery-table/customer-handling/customer-handling.component.ts
@@ -24,10 +24,12 @@ export class CustomerHandlingComponent implements OnInit {
   formEnable: boolean = true;
   display: boolean;
   displayCop: boolean;
+  displayTypedrop: boolean;
   cTypes: typeof CustomerType = CustomerType;
   customerType: CustomerType;
-  InqueryType = [];
-  companyType = [];
+  InqueryTypeArray = [];
+  companyTypeArray = [];
+  customerTypeArray = [];
 
 
   constructor(
@@ -36,15 +38,20 @@ export class CustomerHandlingComponent implements OnInit {
     private formvalidationhelpers: FormValidationHelpers
   ) {
     this.companyCustomerDetails = new CompanyCustomerDeails();
-    this.InqueryType = [
-      {id: InqueryType.Details, name: 'Details'},
+    this.InqueryTypeArray = [
       {id: InqueryType.Quotation, name: 'Quotation'},
+      {id: InqueryType.Details, name: 'Details'},
       {id: InqueryType.Quotation_with_details, name: 'Quotation with Details'}
     ];
 
-    this.companyType = [
+    this.companyTypeArray = [
       {id: CompanyType.Ingenii, name: 'Ingenii'},
       {id: CompanyType.Dimo, name: 'Dimo'},
+    ];
+
+    this.customerTypeArray = [
+      {id: CustomerType.Individual, name: 'Individual'},
+      {id: CustomerType.Corporate, name: 'Corporate'}
     ];
   }
 
@@ -56,12 +63,12 @@ export class CustomerHandlingComponent implements OnInit {
 
   ngOnInit(): void {
     this.individualCorpCustomerForm = this.formbuilder.group({
+      id: [null],
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      nic: [''],
+      nicNumber: [''],
       email: ['', [Validators.required, Validators.email]],
       telNo: ['', [Validators.required, Validators.minLength(10)]],
-      address: ['', [Validators.required]],
       type: ['', [Validators.required]],
       contactPerson: [''],
       contactNo: ['', [Validators.minLength(10)]],
@@ -69,17 +76,18 @@ export class CustomerHandlingComponent implements OnInit {
       companyRegistrationNo: ['', [Validators.required]],
       streetAddressLineOne: ['', [Validators.required]],
       streetAddressLineTwo: ['', [Validators.required]],
-      ppNo: [''],
+      ppNo: '',
       country: ['', [Validators.required]],
       city: ['', [Validators.required]],
       zipCode: ['', [Validators.required]],
       taxNumber: ['', [Validators.required]],
       vatNumber: ['', [Validators.required]],
-      handlingCompany: [this.companyType[0].id, [Validators.required]], /*value by dropdown*/
-      handlingCustomer: [null, [Validators.required]], /*value by dropdown*/
-      inqueryType: ['', [Validators.required]],
+      handlingCompany: [this.companyTypeArray[0].id, [Validators.required]], /*value by dropdown*/
+      handlingCustomer: ['', [Validators.required]], /*value by dropdown*/
+      inqueryType: [InqueryType.Quotation, [Validators.required]],
       customerStatus: ['exist']
     });
+    console.log(this.individualCorpCustomerForm.value.handlingCompany);
     console.log(this.individualCorpCustomerForm.value.customerStatus);
     this.getCustomer(); //send the handlingcompany
   }
@@ -87,32 +95,51 @@ export class CustomerHandlingComponent implements OnInit {
 
   getCustomerStuatus(status, form: FormGroupDirective) { //radio event to select customer status
     if (status.value === 'new') {
-      // form.resetForm();
-      this.individualCorpCustomerForm.reset();
+      console.log(status.value);
+      console.log(this.customerType);
+      this.individualCorpCustomerForm.value.customerStatus = status.value;
+      this.individualCorpCustomerForm.value.handlingCompany = this.companyTypeArray[0].id;
       this.customerType = CustomerType.Individual;
-      this.displayCop = true;
+      this.resetFormControls();
+      this.displayTypedrop = true;
       this.formEnable = false;
-      // console.log(this.formEnable);
     } else {
-      this.displayCop = false;
+      console.log(status.value);
+      // this.displayCop = false;
       this.formEnable = true;
+      this.customerType = this.customers[0].type; //reset custom type to customers[0]
       this.patchToCustomer(this.customers[0]);
+    }
+  }
 
-      // console.log(this.formEnable);
+  getCustomerTypeId(event) {
+    if (event.id === CustomerType.Individual) {
+      this.customerType = CustomerType.Individual;
+      this.individualCorpCustomerForm.value.type = event.id;
+      console.log(this.individualCorpCustomerForm.value.type);
+    } else {
+      this.customerType = CustomerType.Corporate;
+      this.individualCorpCustomerForm.value.type = event.id;
+      console.log(this.individualCorpCustomerForm.value.type);
     }
   }
 
   getCustomerId(event) { //dropdown event when select customer
     this.companyCustomerDetails.id = event.id;
     this.filterCustomerDetails(this.companyCustomerDetails.id);
-
   }
 
   getCompanySelected(event) { //dropdown event
-    this.individualCorpCustomerForm.value.handlingCompany = event.id;  //when event fired need to find the relevant id customer form the array
-    this.allcustomers = [''];
-    this.getCustomer();
-    console.log(this.individualCorpCustomerForm.value.handlingCompany);
+    if (this.individualCorpCustomerForm.value.customerStatus == 'exist') {
+      this.individualCorpCustomerForm.value.handlingCompany = event.id;  //when event fired need to find the relevant id customer form the array
+      this.allcustomers = [''];
+      this.getCustomer();
+      console.log(this.individualCorpCustomerForm.value.handlingCompany);
+    } else {
+      this.individualCorpCustomerForm.value.handlingCompany = event.id;
+      this.allcustomers = [''];
+    }
+
   }
 
   filterCustomerDetails(id) {
@@ -131,12 +158,12 @@ export class CustomerHandlingComponent implements OnInit {
   patchToCustomer(customerValue) {
     console.log(customerValue);
     this.individualCorpCustomerForm.patchValue({
+      id: customerValue.id ? customerValue.id : '',
       firstName: customerValue.firstName ? customerValue.firstName : '',
       lastName: customerValue.lastName ? customerValue.lastName : '',
-      nic: customerValue.nic ? customerValue.nic : '',
+      nicNumber: customerValue.nicNumber ? customerValue.nicNumber : '',
       email: customerValue.email ? customerValue.email : '',
       telNo: customerValue.telNo ? customerValue.telNo : '',
-      address: customerValue.address ? customerValue.address : '',
       type: customerValue.type ? customerValue.type : '',
       companyName: customerValue.companyName ? customerValue.companyName : '',
       companyRegistrationNo: customerValue.companyRegistrationNo ? customerValue.companyRegistrationNo : '',
@@ -162,12 +189,11 @@ export class CustomerHandlingComponent implements OnInit {
       respond => {
         this.customers = respond.data;
         this.customers.forEach(i => {
-          this.allcustomers.push({id: i.id, name: i.firstName ? i.firstName : ''});
+          this.allcustomers.push({id: i.id, name: i.firstName ? i.firstName : i.companyName});
         });
         this.showCustomers = this.allcustomers;
-        // console.log(this.customers[0]);
-        //when page load first array element get
-        this.patchToCustomer(this.customers[0]); // call to patch the first customer
+        this.customerType = this.customers[0].type;
+        this.patchToCustomer(this.customers[0]);
       }, error => {
 
       });
@@ -186,7 +212,36 @@ export class CustomerHandlingComponent implements OnInit {
     }
   }
 
-  udpateInquery(id) {
+  resetFormControls() {
+      this.individualCorpCustomerForm.patchValue({
+        id: null,
+        firstName: null,
+        lastName: null,
+        nicNumber: null,
+        email: '',
+        telNo: '',
+        type: this.customerTypeArray[0].id,
+        contactPerson: '',
+        contactNo: '',
+        companyName: '',
+        companyRegistrationNo: '',
+        streetAddressLineOne: '',
+        streetAddressLineTwo: '',
+        ppNo: '',
+        country: '',
+        city: '',
+        zipCode: '',
+        taxNumber: '',
+        vatNumber: '',
+        handlingCompany: this.companyTypeArray[0].id,
+        handlingCustomer: '',
+        inqueryType: InqueryType.Quotation,
+        customerStatus: 'new'
+      });
+  }
+
+  get id() {
+    return this.individualCorpCustomerForm.get('id');
   }
 
   get firstName() {
@@ -197,8 +252,8 @@ export class CustomerHandlingComponent implements OnInit {
     return this.individualCorpCustomerForm.get('lastName');
   }
 
-  get nic() {
-    return this.individualCorpCustomerForm.get('nic');
+  get nicNumber() {
+    return this.individualCorpCustomerForm.get('nicNumber');
   }
 
   get ppNo() {
@@ -211,10 +266,6 @@ export class CustomerHandlingComponent implements OnInit {
 
   get telNo() {
     return this.individualCorpCustomerForm.get('telNo');
-  }
-
-  get address() {
-    return this.individualCorpCustomerForm.get('address');
   }
 
   get companyName() {
